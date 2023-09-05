5a7c2c2383d72f60005235096ebcf9c2f337c328
jonghwan <subdiary@daum.net>
2023-09-05 15:15:48 +0900

    input

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,46 @@
 <template>
-    적용 테스트03
-    <HelloWorld msg="Welcome to Your Vue.js App.."/>
-    <ul>
-        <li>
-            <Inputs :_type="Inputs_data[0].type" :_id="Inputs_data[0].id" :_name="Inputs_data[0].name" class="ani" />
-            {{ Inputs_data[0].txt }}
-        </li>
-        <!-- 특정 위치 부터 가져올 때 method에 선언한 함수(startFrom) 호출 -->
-        <!-- <li v-for="(e,i) in Inputs_data" :key="i">
-            <Inputs :_type="e.type" :_id="e.id" :_name="e.name" />
-            {{ e.txt }}
-            {{ i }}
-        </li> -->
-        <li v-for="(e,i) in startFrom(Inputs_data, 1,3)" :key="i">
-            <!-- <Inputs :_type="e.type" :_id="e.id" :_name="e.name" /> -->
-            a : 
-            {{ e.txt }}
-        </li>
-        <li v-for="(e,i) in startFrom(Inputs_data, 3,5)" :key="i">
-            <!-- <Inputs :_type="e.type" :_id="e.id" :_name="e.name" /> -->
-            b : 
-            {{ e.txt }}
-            {{ i }}
-        </li>
-    </ul>
+    <div>
+        <section>
+            <h2>Inputs</h2>
+            <ul class="explain">
+                <li>Input type 선택 : _type</li>
+                <li>스위치 타입 : :isswitch='true'</li>
+            </ul>
+            <div class="design_box">
+                <ul>
+                    <!-- 특정 위치 부터 가져올 때 method에 선언한 함수(startFrom) 호출 -->
+                    <li v-for="(e,i) in startFrom(Inputs_data, 1)" :key="i" style="width:33.3333%;">
+                        <Inputs :_type="e.type" :_id="e.id" :_name="e.name" :_text="e.txt" :isswitch="e.isswitch" :_placeholder="e.placeholder" />
+                    </li>
+                </ul>
+            </div>
+            <div class="code_box">
+                <pre><code>&lt;Inputs _type="radio" _id="id" _name="name" _text="Text" /&gt;
+&lt;Inputs _type="checkbox" _id="id" _name="name" _text="Text" /&gt;
+&lt;Inputs _type="checkbox" _id="id" _name="name" _text="Text" isswitch="true" /&gt;
+&lt;Inputs _type="text" _id="id" _name="name" _text="Text" _placeholder="placeholder" /&gt;</code></pre>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Inputs from './components/Inputs.vue'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld,
         Inputs
     },
     data(){
         return {
             Inputs_data:[
                 {type:'radio', id:'first', name:'first', txt:'First Item'},
-                {type:'radio', id:'test1', name:'test1', txt:'Second Item'},
-                {type:'checkbox', id:'test2', name:'test2', txt:'Third Item'},
-                {type:'radio', id:'test1', name:'test1', txt:'Fourth Item'},
-                {type:'checkbox', id:'test2', name:'test2', txt:'Fiveth Item'}
+                {type:'radio', id:'test1', name:'test1', txt:'Radio'},
+                {type:'checkbox', id:'test2', name:'test2', txt:'Checkbox'},
+                {type:'checkbox', id:'test2', name:'test2', txt:'Switch', isswitch:true},
+                {type:'text', id:'test2', name:'test2', placeholder:'Text'},
+                {type:'password', id:'test2', name:'test2', placeholder:'Password'}
             ]
         }
     },
@@ -56,13 +53,10 @@ export default {
 }
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+<style scoped>
+section {margin:20px;}
+.design_box > ul {margin-top:-15px; margin-left:-15px; display:flex; flex-wrap:wrap; align-items:center;}
+.design_box > ul > li {padding-top:15px; padding-left:15px;}
+
+.code_box {margin-top:20px; padding:15px; color:#fff; line-height:1.8; background-color:#000;}
 </style>
--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -1,20 +1,110 @@
 <template>
-    <label v-if="_type == 'checkbox' || _type == 'radio'" class="check">
+    <label v-if="_type == 'checkbox' || _type == 'radio'" :class="{'check': !isswitch, 'check_ani': isswitch}">
         <input :type="_type" :id="_id" :name="_name" />
+        <em class="label">{{_text}}</em>
     </label>
+    <div v-else class="label_wrap">
+        <label class="input" :class="{'err':isError}">
+            <i v-if="_placeholder && !isDisabled">{{_placeholder}}</i>
+            <input :type="_type" :id="_id" :value="_value" @focus="hidePlaceholder" @blur="valueChk" @keyup="input_btn_chk" :readonly="isReadonly" :disabled="isDisabled" />
+            <em class="err_txt">{{err_text}}</em>
+            <button class="icon_del" v-if="_type != 'password'" @click="input_btn_fn">Delete</button>
+            <button class="icon_pass" v-if="_type == 'password'" @click="input_btn_fn">password</button>
+        </label>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Inputs',
     props: {
-        _id: String,
-        _name: String,
-        _type: String
+        _id: String, //input ID
+        _name: String, // Input NAME
+        _type: String, // Input TYPE
+        _text: String, // checkbox, radio 문구ㅜ
+        _placeholder: String, // Input PLACEHOLDER
+        _value: null, // Input VALUE
+        _err_text: String, // 오류 시 노출 문구
+        isError: false, // 오류 여부
+        isReadonly: false, // readonly 여부
+        isDisabled:false, // disabled 여부
+        isswitch: false, // checkbox switch 형태
+        isRrequired: false, // 필수값 여부
+    },
+    methods:{
+        hidePlaceholder(e){ //focus시 placeholder 숨김
+            e.currentTarget.previousSibling.style.display="none"
+        },
+        valueChk(e){ //blur 시 value 유무 확인하여 placeholder 조절
+            console.log(e.currentTarget.value.length);
+            if(e.currentTarget.value.length > 0){
+                e.currentTarget.previousSibling.style.display='none';
+            } else {
+                e.currentTarget.previousSibling.style.removeProperty('display');
+            }
+        },
+        input_btn_chk(e){ //keypress 시 value 유무 확인하여 버튼 노출 조절
+            var icon_button = e.currentTarget.closest('.label_wrap').querySelector('button');
+            if(e.currentTarget.value.length > 0){
+                icon_button.style.cssText="display:block;"
+            }else{
+                icon_button.style.cssText="display:none;"
+            }
+        },
+        input_btn_fn(e){ //label_wrap의 버튼 클릭 시 각 기능
+            var icon_button = e.currentTarget.closest('.label_wrap').querySelector('button');
+            if(icon_button.getAttribute('class') == 'icon_del'){ //value 삭제
+                e.currentTarget.closest('.label_wrap').querySelector('input').value = null;
+                icon_button.style.cssText="display:none;"
+                e.currentTarget.closest('.label_wrap').querySelector('i').style.display='block';
+            } else if(icon_button.getAttribute('class') == 'icon_pass') { //비밀번호 보기
+                e.currentTarget.closest('.label_wrap').querySelector('input').setAttribute('type','text');
+                icon_button.setAttribute('class', 'icon_text');
+            } else { //비밀번호 숨기기
+                e.currentTarget.closest('.label_wrap').querySelector('input').setAttribute('type','password');
+                icon_button.setAttribute('class', 'icon_pass');
+            }
+        }
     }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+em {font-style:normal;}
+.check {min-height:20px; padding-left:26px; line-height:1; position:relative; display:inline-block;}
+.check > * {line-height:1.4; vertical-align:middle; display:inline-block;}
+.check input {margin:0 0 0 1px; position:absolute;}
+.check .label:before, .check .label:after {content:''; position:absolute; display:inline-block;}
+
+/* radio */
+.check input[type='radio'], .check input[type='checkbox'] {opacity:0;}
+.check input[type='radio'], .check input[type='radio'] + .label:before {width:20px; height:20px; background-color:#fff; border:1px solid #ccc; border-radius:50%; box-sizing:border-box; top:3px; left:0;}
+.check input[type='radio']:checked + .label:after {width:14px; height:14px; background-color:#333; border-radius:50%; box-sizing:border-box; top:6px; left:3px;}
+
+/* checkbox */
+.check input[type='checkbox'], .check input[type='checkbox'] + .label:before {width:20px; height:20px; background-color:#fff; border:1px solid #ccc; box-sizing:border-box; top:0; left:0;}
+.check input[type='checkbox']:checked + .label:after {width:6px; height:10px; border:3px solid #333; border-top:0; border-left:0; top:1px; left:6px; -webkit-transform:rotate(45deg); transform:rotate(45deg);}
+
+.check_ani {position:relative; display:inline-block;}
+.check_ani input {margin:0 0 0 1px; position:absolute; opacity:0;}
+.check_ani input[type='checkbox'] {opacity:0;}
+.check_ani .label {min-height:22px; padding-left:48px; position:relative; display:block;}
+.check_ani .label:before {width:42px; height:22px; background-color:#ccc; border:0; border-radius:60px; content:''; position:absolute; top:0; left:0; display:inline-block;}
+.check_ani input[type='checkbox'] + .label:after {width:18px; height:18px; background-color:#fff; border-radius:50%; content:''; position:absolute; top:50%; left:2px; z-index:1; display:block; transform:translateY(-50%); transition:all 0.2s;}
+.check_ani input[type='checkbox']:checked + .label:before {background-color:#ff7200;}
+.check_ani input[type='checkbox']:checked + .label:after {left:22px;}
+
+label.input, label.select > div {position:relative; display:block;}
+label.input i {color:#aaa; font-size:16px; line-height:18px; position:absolute; top:50%; left:15px; transform:translateY(-50%);}/* placeholder */
+label.input input[type='text'], label.input input[type='password'], label.input input[type='number'] {width:100%; font-size:16px; font-family:'MyLotte'; line-height:18px;}
+label.input .err_txt {margin-top:5px; color:#ff7200; font-size:12px; font-family:'Noto Sans'; line-height:18px; display:none;}
+label.input.err .err_txt {display:block;}
+label.input * {flex:1 auto;}
+
+.label_wrap {position:relative; flex:1;}
+.label_wrap button[class*='icon_'] {width:24px; height:24px; font-size:0; background-repeat:no-repeat; border-radius:50%; position:absolute; top:9px; right:15px; z-index:1; display:none;}
+.label_wrap button[class*='icon_'] + button[class*='icon_'] {margin-right:34px;}
+.label_wrap button[class*='icon_'] {background-image:url('@/assets/images/common/icon_split.png'); background-repeat:no-repeat; background-size:150px auto;}
+.label_wrap button.icon_del {background-position:0 0;}
+.label_wrap button.icon_pass {background-position:-80px 0;}
+.label_wrap button.icon_text {background-position:-40px 0;}
 </style>
